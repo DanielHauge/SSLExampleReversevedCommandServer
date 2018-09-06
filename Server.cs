b702a5f980aedd9bdeb35e16bc199f3cf8b91167
Daniel Hauge <Animcuil@gmail.com>
2018-09-06 17:27:27 +0200

    Example of working reversed server-client architechture, with blind trust in 'client' which is realy the server

---
+++ Server.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        const int port = 5000;
+        const string server_ip = "127.0.0.1";
+
+
+        static void Main(string[] args)
+        {
+            var client = new TcpClient(server_ip, port);
+            var keystore = new X509Store(StoreLocation.LocalMachine);
+            keystore.Open(OpenFlags.ReadOnly);
+            var allcerts = keystore.Certificates;
+            var certs = keystore.Certificates.Find(X509FindType.FindBySerialNumber, "7582af584e2c2199485c15f1fd02d184", false);
+            using (SslStream sslStream = new SslStream(client.GetStream(), false, new RemoteCertificateValidationCallback(ValidateClientCertificate), null))
+            {
+                sslStream.AuthenticateAsServer(certs[0]);
+                sslStream.Write(Encoding.ASCII.GetBytes("Hello"));
+            }
+        }
+
+        private static bool ValidateClientCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+    }
+}
---
+++ Tray.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting tray");
+            
+            var server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            var client = server.AcceptTcpClient();
+
+            using (var sslStream = new SslStream(client.GetStream(), false, new RemoteCertificateValidationCallback(ValidateCertificate)))
+            {
+                sslStream.AuthenticateAsClient("127.0.0.1");
+                byte[] buffer = new byte[2048];
+                StringBuilder messageData = new StringBuilder();
+                int bytes = -1;
+                do
+                {
+                    bytes = sslStream.Read(buffer, 0, buffer.Length);
+
+                    Decoder decoder = Encoding.UTF8.GetDecoder();
+                    char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                    decoder.GetChars(buffer, 0, bytes, chars, 0);
+                    messageData.Append(chars);
+
+                    if (messageData.ToString().IndexOf("<EOF>") != -1)
+                    {
+                        break;
+                    }
+                } while (bytes != 0);
+
+                Console.Write(messageData.ToString());
+                Console.ReadKey();
+            }
+            
+
+        }
+
+        static bool ValidateCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true;
+        }
+    }
+}
